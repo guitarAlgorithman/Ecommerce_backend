827aa30ef654294497fef5ebc15a21cd4335b5f8
sauriokat <gustavo.henriquez.m@gmail.com>
2023-02-15 19:47:35 -0300

    pre revision 6

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 
+const indexRouter = require("./index.routes");
 const productRouter = require("./product.routes");
 const userRouter = require("./user.routes");
 const compraRouter = require("./compra.routes");
 
+router.use("", indexRouter);
 router.use("/products", productRouter);
 router.use("/user", userRouter);
 router.use("/comprar", compraRouter);
